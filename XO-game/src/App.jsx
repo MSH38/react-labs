e4202e4a39f4ef0de3bd75185ebf9cd4e05fc2e3
Mahmoud Heikal <m.samy1011997@gmail.com>
2025-12-13 15:32:37 +0200

    editable input

--- XO-game/src/App.jsx
+++ XO-game/src/App.jsx
@@ -1,10 +1,15 @@
-
+import Player from "./components/Player.jsx"
 function App() {
-  
-
-  // return (
-  //   // <h1>React XO Game</h1>
-  // )
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <Player intialName="player 1" symbol="X" />
+        <Player intialName="player 2" symbol="O" />
+      </ol>
+      Game Over
+    </div>
+    LOG
+  </main>
 }
 
 export default App
